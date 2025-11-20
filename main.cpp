24b728fb8a3864dd4e6e58dabef17a0646505c78
vsevolod <jhonykattvil@gmail.com>
2025-11-20 19:23:03 +0300

    add project skeleton

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "substring_search.hpp"
+#include "bracket_balance.hpp"
+
+int main() {
+    std::cout << "SIMD Text Processing\n";
+
+    SubstringSearch ss;
+    BracketBalance bb;
+
+    // позже добавим запуск и тестирование
+    std::cout << "Project structure OK\n";
+
+    return 0;
+}
